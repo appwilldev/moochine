41b655e0b59b87ca9c549b4bf1888c265d91bf5c
KDr2 <killy.draw@gmail.com>
2012-02-10 10:30:16 +0800

    set indent-level to 4

--- luasrc/mch/router.lua
+++ luasrc/mch/router.lua
@@ -10,21 +10,21 @@ module('router',package.seeall)
 
 
 function map(route_table, uri, func_name)
-   local mod,fn = string.match(func_name,'^(.+)%.([^.]+)$')
-   mod=require(mod)
-   route_table[uri]=mod[fn]
+    local mod,fn = string.match(func_name,'^(.+)%.([^.]+)$')
+    mod=require(mod)
+    route_table[uri]=mod[fn]
 end
 
 
 function setup(app_name)
-   app_name='MOOCHINE_APP_' .. app_name
-   if not _G[app_name] then
-      _G[app_name]={}
-   end
-   local route_map={}
-   _G[app_name]['route_map']=route_map
-   _G[app_name]['map']=functional.curry(map,route_map)
-   setfenv(2,_G[app_name])
+    app_name='MOOCHINE_APP_' .. app_name
+    if not _G[app_name] then
+        _G[app_name]={}
+    end
+    local route_map={}
+    _G[app_name]['route_map']=route_map
+    _G[app_name]['map']=functional.curry(map,route_map)
+    setfenv(2,_G[app_name])
 end
 
 
--- luasrc/mch_content.lua
+++ luasrc/mch_content.lua
@@ -6,39 +6,39 @@
 
 
 function setup_app()
-   local app_path = ngx.var.MOOCHINE_APP
-   local mch_home = ngx.var.MOOCHINE_HOME
-   _G['MOOCHINE_APP']=string.match(app_path,'^.*/([^/]+)/?$')
-   package.path = mch_home .. '/luasrc/?.lua;' .. package.path
-   package.path = app_path .. '/app/?.lua;' .. package.path
-   require("mch.request")
-   require("mch.response")
-   _G['MOOCHINE_MODULES']={}
-   _G['MOOCHINE_MODULES']['request']=request
-   _G['MOOCHINE_MODULES']['response']=response
-   require("routing")
+    local app_path = ngx.var.MOOCHINE_APP
+    local mch_home = ngx.var.MOOCHINE_HOME
+    _G['MOOCHINE_APP']=string.match(app_path,'^.*/([^/]+)/?$')
+    package.path = mch_home .. '/luasrc/?.lua;' .. package.path
+    package.path = app_path .. '/app/?.lua;' .. package.path
+    require("mch.request")
+    require("mch.response")
+    _G['MOOCHINE_MODULES']={}
+    _G['MOOCHINE_MODULES']['request']=request
+    _G['MOOCHINE_MODULES']['response']=response
+    require("routing")
 end
 
 function content()
-   if not _G['MOOCHINE_APP'] then
-      setup_app()
-   end
-   if not _G['MOOCHINE_APP'] then
-      ngx.say('Can not setup MOOCHINE APP')
-      ngx.exit(501)
-   end
-   local uri=ngx.var.REQUEST_URI
-   local app_env_key='MOOCHINE_APP_' .. _G['MOOCHINE_APP']
-   local route_map=_G[app_env_key]['route_map']
-   for k,v in pairs(route_map) do
-      local args=string.match(uri, k)
-      if args then
-         local request=_G['MOOCHINE_MODULES']['request']
-         local response=_G['MOOCHINE_MODULES']['response']
-         v(request.Request:new(),response.Response:new(),args)
-         break
-      end
-   end
+    if not _G['MOOCHINE_APP'] then
+        setup_app()
+    end
+    if not _G['MOOCHINE_APP'] then
+        ngx.say('Can not setup MOOCHINE APP')
+        ngx.exit(501)
+    end
+    local uri=ngx.var.REQUEST_URI
+    local app_env_key='MOOCHINE_APP_' .. _G['MOOCHINE_APP']
+    local route_map=_G[app_env_key]['route_map']
+    for k,v in pairs(route_map) do
+        local args=string.match(uri, k)
+        if args then
+            local request=_G['MOOCHINE_MODULES']['request']
+            local response=_G['MOOCHINE_MODULES']['response']
+            v(request.Request:new(),response.Response:new(),args)
+            break
+        end
+    end
 end
 
 content()
