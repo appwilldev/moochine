b77bd81ddf745e69bebb603ac15e32a3f6360bcd
lilydjwg <lilydjwg@gmail.com>
2012-08-06 17:54:54 +0800

    __logger should be available in routing.lua after router setup

--- luasrc/mch/router.lua
+++ luasrc/mch/router.lua
@@ -52,6 +52,7 @@ function setup()
         mch.vars.get(app_name,"ROUTE_INFO")['ROUTE_MAP']={}
         mch.vars.get(app_name,"ROUTE_INFO")['ROUTE_ORDER']={}
     end
+    mch.vars.get(app_name, "ROUTE_INFO").__logger = __logger
     mch.vars.get(app_name, "ROUTE_INFO")['map'] = mch.functional.curry(
         map,
         mch.vars.get(app_name,"ROUTE_INFO")['ROUTE_MAP'],
