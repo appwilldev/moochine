1754062f67a33f0fe2f2561ca1f94f96c21aae8e
lilydjwg <lilydjwg@gmail.com>
2012-08-07 10:28:34 +0800

    reshape and add shebang to shell script

--- rudiment/bin/start.sh
+++ rudiment/bin/start.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 # start nginx in app
 
 NGINX_RUNTIME="nginx_runtime"
-APP_PATH=`pwd`
+APP_PATH=$PWD
 APP_NAME=`basename $(APP_PATH)`
 
-mkdir -p $NGINX_RUNTIME
-mkdir -p $NGINX_RUNTIME"/logs"
+mkdir -p "$NGINX_RUNTIME/logs"
 
 #export OPENRESTY_HOME=/usr/local/openresty
 #export MOOCHINE_HOME=/YOUR PATH/moochine
@@ -14,15 +14,13 @@ mkdir -p $NGINX_RUNTIME"/logs"
 
 MOOCHINE_APP_EXTRA=$PWD
 
-cat conf/nginx.conf | sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|"\
-                    | sed -e "s|__MOOCHINE_APP_PATH_VALUE__|$APP_PATH|"\
-                    | sed -e "s|__MOOCHINE_APP_NAME_VALUE__|$APP_NAME|"\
-                    | sed -e "s|__HADDIT_APP_CONFIG__|$HADDIT_APP_CONFIG|"\
-                    > $NGINX_RUNTIME/conf/p-nginx.conf
+sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|" \
+    -e "s|__MOOCHINE_APP_PATH_VALUE__|$APP_PATH|" \
+    -e "s|__MOOCHINE_APP_NAME_VALUE__|$APP_NAME|" \
+    -e "s|__HADDIT_APP_CONFIG__|$HADDIT_APP_CONFIG|" \
+    cat conf/nginx.conf > $NGINX_RUNTIME/conf/p-nginx.conf
 
 #$OPENRESTY_HOME/nginx/sbin/nginx -p `pwd`/ -c conf/p-nginx.conf
 $OPENRESTY_HOME/nginx/sbin/nginx -p $NGINX_RUNTIME/ -c conf/p-nginx.conf
 
 echo "start nginx!"
-
-
