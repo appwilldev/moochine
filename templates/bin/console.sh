797136b3563890c44233fc399d1d837b04f9238b
KDr2 <killy.draw@gmail.com>
2013-05-20 16:41:22 +0800

    auto get absolute path for mch home and mch app path

--- templates/bin/console.sh
+++ templates/bin/console.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
+source `dirname $0`/mch-vars.sh
 $MOOCHINE_HOME/bin/mchconsole http://localhost:9800/mchconsole
-
--- templates/bin/generate_nginx_conf.sh
+++ templates/bin/generate_nginx_conf.sh
@@ -2,20 +2,21 @@
 
 # set global variables
 #export OPENRESTY_HOME=/usr/local/openresty
-#export MOOCHINE_HOME=/your/path/to/moochine
+
+source `dirname $0`/mch-vars.sh
 
 PWD=`pwd`
 
-NGINX_FILES=$PWD"/nginx_runtime"
+NGINX_FILES=$MOOCHINE_APP_PATH"/nginx_runtime"
 
-mkdir -p $PWD"/logs"
+mkdir -p $MOOCHINE_APP_PATH"/logs"
 mkdir -p $NGINX_FILES"/conf"
 mkdir -p $NGINX_FILES"/logs"
 
-cp $PWD"/conf/mime.types" $NGINX_FILES"/conf/"
+cp $MOOCHINE_APP_PATH"/conf/mime.types" $NGINX_FILES"/conf/"
 
 sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|" \
-    -e "s|__MOOCHINE_APP_PATH_VALUE__|$PWD|" \
+    -e "s|__MOOCHINE_APP_NAME_VALUE__|$MOOCHINE_APP_NAME|" \
+    -e "s|__MOOCHINE_APP_PATH_VALUE__|$MOOCHINE_APP_PATH|" \
     -e "s|__PWD__|$PWD|" \
-    $PWD/conf/nginx.conf > $NGINX_FILES/conf/p-nginx.conf
-
+    $MOOCHINE_APP_PATH/conf/nginx.conf > $NGINX_FILES/conf/p-nginx.conf
